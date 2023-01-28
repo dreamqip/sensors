705457bf003b1b7ff01f692faa18ccf85645d6b6
Vladyslav Chuchupak <v.chuchupak@gmail.com>
2023-01-28 21:10:37 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+import { Accelerometer, Gyroscope, Magnetometer } from 'expo-sensors';
+import { useEffect, useState } from 'react';
+import { useButchesStore } from './store';
+import Sensor from './components/Sensor';
 
 export default function App() {
+  const [start, setStart] = useState(false);
+  const [time, setTime] = useState(0);
+  const { butchesCount, reset } = useButchesStore();
+
+  const _toggle = () => {
+    if (start) {
+      Alert.alert('Stop', 'Do you want to stop sensors?', [
+        { text: 'No', style: 'cancel' },
+        { text: 'Yes', onPress: () => setStart(false) },
+      ]);
+    } else {
+      setStart(!start);
+    }
+  };
+
+  useEffect(() => {
+    if (start) {
+      const interval = setInterval(() => {
+        setTime(time => time + 1);
+      }, 1000);
+      return () => {
+        clearInterval(interval);
+        setTime(0);
+        reset();
+      };
+    }
+  }, [start]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
+      <Sensor title='Accelerometer' sensor={Accelerometer} start={start} />
+      <Sensor title='Gyroscope' sensor={Gyroscope} start={start} />
+      <Sensor title='Magnetometer' sensor={Magnetometer} start={start} />
+      <Button title={start ? 'Stop' : 'Start'} onPress={_toggle} />
+      <View style={styles.rideInfo}>
+        <View style={styles.rideInfoItem}>
+          <Text style={styles.rideInfoItemText}>Butches count: {butchesCount}</Text>
+        </View>
+        <View style={styles.rideInfoItem}>
+          <Text style={styles.rideInfoItemText}>
+            Ride time: {time}
+          </Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +58,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  rideInfo: {
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: 300,
+  },
+  rideInfoItem: {
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 6,
+    borderWidth: 2,
+    borderColor: '#60a5fa',
+  },
+  rideInfoItemText: {
+    fontSize: 16,
+    fontWeight: '500',
+    color: '#ffffff',
   },
 });
